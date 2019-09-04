0320d7e58665c01d8706655aadb540bd07225704
Tayler Johnson <johnsontayler@me.com>
2019-09-04 12:08:22 -0300

    new controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/:id', to: 'tasks#show'
+
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id', to: 'tasks#update'
+
+  get 'tasks/:id', to: 'tasks#destroy'
 end
